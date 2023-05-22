b98994ba27c7180426c85b508df34feba3310952
Tatyana58 <tatbir@rambler.ru>
2023-05-22 21:24:53 +0300

    Начало Создание нескольких методов контроллера

---
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary/controller/FirstController.java
@@ -0,0 +1,22 @@
+package ru.skypro.lessons.springboot.weblibrary.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class FirstController {
+    private int counter;
+
+    @GetMapping
+    public String showHelloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/counter")
+    public String showCounter() {
+        return "Количество запросов: " + ++counter;
+        // Увеличиваем счетчик и выводим его значение
+    }
+}
